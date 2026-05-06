1efc31130aa1d526cc6facdc8ba3fdd2ee59fa6c
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-05-06 22:58:13 +0530

    packed_flags solutions

--- intermediate/src/hard/packed_flags.rs
+++ intermediate/src/hard/packed_flags.rs
@@ -15,19 +15,32 @@ pub struct PackedFlags {
 }
 
 impl PackedFlags {
+    // all flags = false (0)
     pub fn new() -> Self {
-        todo!()
+        PackedFlags { bits: 0 }
     }
 
+    // set or clear a bit at index
     pub fn set(&mut self, index: u8, value: bool) {
-        todo!()
+        let mask = 1 << index;
+
+        if value {
+            // set bit (make 1)
+            self.bits |= mask;
+        } else {
+            // clear bit (make 0)
+            self.bits &= !mask;
+        }
     }
 
+    // read a bit at index
     pub fn get(&self, index: u8) -> bool {
-        todo!()
+        let mask = 1 << index;
+        (self.bits & mask) != 0
     }
 
+    // return raw byte
     pub fn as_byte(&self) -> u8 {
-        todo!()
+        self.bits
     }
 }
